b759145a41ad9ef3381defb503b84de1e86829d5
20040259 <20040259@mydbs.ie>
2024-12-14 15:46:57 +0530

    Add files via upload

---
+++ Cricket_Auction.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Cricket_Auction.Web.MvcApplication" Language="C#" %>
---
+++ Cricket_Auction.Web/Startup.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Threading.Tasks;
+
+
+[assembly: OwinStartup(typeof(Cricket_Auction.Web.Startup))]
+
+namespace Cricket_Auction.Web
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+       
+        
+        }
+
+       
+    }
+}
