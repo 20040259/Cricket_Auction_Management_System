7333cdd251b75c84ae2804087ee2b0c41530c383
20040259 <20040259@mydbs.ie>
2024-12-14 15:45:59 +0530

    Add files via upload

---
+++ Cricket_Auction.Web/ViewModels/CategoriesViewModels.cs
@@ -0,0 +1,36 @@
+﻿using Cricket_Auction.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cricket_Auction.Web.ViewModels
+{
+    public class CategoriesListingViewModel : PageViewModel
+    {
+        public List<Category> Categories { get; set; }
+    }
+
+    public class CategoriesViewModel : PageViewModel
+    {
+        public List<Category> AllCategories { get; set; }
+    }
+
+    public class CategoryDetailsViewModel : PageViewModel
+    {
+        public Category Category { get; set; }
+    }
+
+    public class CategoryViewModel : PageViewModel
+    {
+        public int ID { get; set; }
+        public string TeamName { get; set; }
+        public string TeamDescription { get; set; }
+        public double MaxPrice { get; set; }
+
+        public List<Auction> Auctions { get; set; }
+
+    }
+
+   
+}
---
+++ Cricket_Auction.Web/ViewModels/ManagePlayerViewModel.cs
@@ -0,0 +1,77 @@
+﻿using Cricket_Auction.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Cricket_Auction.Web.ViewModels
+{
+    public class ManagePlayersListingViewModel : PageViewModel
+    {
+        public List<Players> players { get; set; }
+    }
+
+    public class ManagePlayersViewModel : PageViewModel
+    {
+        public List<Players> AllPlayers { get; set; }
+    }
+
+    public class PlayerDetailsViewModel : PageViewModel
+    {
+        public Players Players { get; set; }
+    }
+
+    public class ManagePlayerViewModel : PageViewModel
+    {
+        public int TrophyID { get; set; }
+       
+        public int ID { get; set; }
+
+
+        //[DisplayName("Upload photo")]
+        //  public string PictureURL { get; set; }
+
+        //validations
+        [Required]
+       // [StringLength(50, MinimumLength = 3, ErrorMessage = "First name must be minimum of 3 letters")]
+        public string FirstName { get; set; }
+
+        [Required]
+       // [StringLength(50, MinimumLength = 3, ErrorMessage = "Last name must be minimum of 3 letters")]
+        public string LastName { get; set; }
+
+        [Required]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "User name must be minimum of 3 letters")]
+        public string Username { get; set; }
+
+        [Required]
+        [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}", ErrorMessage = "Please Enter Valid Email ID")]
+        public string Email { get; set; }
+
+        [Required]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,15}$", ErrorMessage = "Password must be minimum of 8 characters include numbers,letters")]
+        public string Password { get; set; }
+
+
+        [Required]
+        public string Gender { get; set; }
+
+        [Required]
+        public string PlayerRole { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        public List<Trophy> Trophies { get; set; }
+
+        //  public string PlayerPictures { get; set; }
+
+        //use to get full name of player
+        //public string PlayerName()
+        //{
+        //    return this.FirstName + " " + LastName;
+        //}
+        //  public List<PlayerPictures> PlayerPicturesList { get; set; }
+    }
+}
