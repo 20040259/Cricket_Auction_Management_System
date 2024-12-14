e79ab12a1714086d7e8bfbc41e6ac621b93cd37f
20040259 <20040259@mydbs.ie>
2024-12-14 16:49:39 +0530

    Add files via upload

---
+++ Cricket_Auction.Data/Cricket_Auction_Context.cs
@@ -0,0 +1,23 @@
+﻿using Cricket_Auction.Entities;
+using System.Data.Entity;
+
+
+namespace Cricket_Auction.Data
+{
+    public class Cricket_Auction_Context : DbContext
+    {
+        public Cricket_Auction_Context() : base("name=CricketAuctionConnectionString")
+        {
+        }
+
+        public DbSet<Trophy> Trophies { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Auction> Auctions { get; set; }
+        public DbSet<Picture> Pictures { get; set; }
+        public DbSet<AuctionPicture> AuctionPictures { get; set; }
+        public DbSet<Players> Players { get; set; }
+        public DbSet<AccountAccess> Account { get; set; }
+        public DbSet<TeamOwner> TeamOwner { get; set; }
+
+    }
+}
